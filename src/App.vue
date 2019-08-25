c9f02e9d568be13dacfbcbb3403dbb71c2c0c5fe
zztdan <zztdandan@gmail.com>
2019-08-25 21:29:16 +0800

    引用element-ui成功

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <el-button>element按钮测试</el-button>
     </div>
     <router-view/>
   </div>
